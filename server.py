2b8a3b065ee6c7b9d8d69a95858e1b1020931c7d
Shalini Taknet <shalini.mdcomm@gmail.com>
2024-08-31 19:56:20 +0530

    Added all untracked files

--- server.py
+++ server.py
@@ -12,12 +12,15 @@ def index():
 @app.route('/weather')
 def get_weather():
     city = request.args.get('city')
+    if not bool(city.strip()):
+        city = "New Delhi"
     weather_data = get_current_weather(city)
     return render_template(
         "weather.html",
         title = weather_data["name"],
-        status=weather_data["weather"][0]["description"].capitalize(),
-        temp=f'{weather_data['main']['temp']:.1f}', feels_like=f'{weather_data['main']['feels_like']:.1f}'
+        status = weather_data["weather"][0]["description"].capitalize(),
+        temp = f"{weather_data['main']['temp']:.1f}",
+        feels_like = f"{weather_data['main']['feels_like']:.1f}"
     )
 
 if __name__ == "__main__":
--- weather.py
+++ weather.py
@@ -6,7 +6,7 @@ import os
 load_dotenv()
 
 def get_current_weather(city="Kansas City"):
-    request_url= f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+    request_url= f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
 
     weather_data = requests.get(request_url).json()
 
@@ -15,7 +15,9 @@ def get_current_weather(city="Kansas City"):
 if __name__ == "__main__":
     print('\n***Get Weather Conditions***')
     city = input("\nPlease enter a city name: ")
+    if not bool(city.strip()):
+        city = "New Delhi"
     weather_data = get_current_weather(city)
     print("\n")
     pprint(weather_data)
-    
+
